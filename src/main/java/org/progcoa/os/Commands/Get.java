b6a921fca6ddbdeb1303e46f374ba4bec05ef5c4
SHHIKA <miaivalse@gmail.com>
2022-05-20 19:00:36 +0900

    First Release ver 1.0.0

--- src/main/java/org/progcoa/os/Commands/Get.java
+++ src/main/java/org/progcoa/os/Commands/Get.java
@@ -1,10 +1,17 @@
 package org.progcoa.os.Commands;
 
+import org.apache.commons.io.FileUtils;
 import org.progcoa.os.Commands.Frame.CommandInterface;
 import org.progcoa.os.Lib.Console;
 import org.progcoa.os.Main;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Set;
 
 public class Get extends CommandInterface {
@@ -22,6 +29,22 @@ public class Get extends CommandInterface {
             return;
         }
 
-        //URLからDLする処理
+        try {
+            download((String) Main.releases.get(args[0]), args[0]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void download(String FileUrl, String path) throws IOException {
+        Console.Log("getting file: " + FileUrl);
+        Console.Log("download file");
+        Console.Log("Please wait...");
+
+        URL url = new URL(FileUrl);
+        File destFile = new File("downloads/" + path + ".zip");
+        FileUtils.copyURLToFile(url, destFile);
+
+        Console.Log("DOWNLOAD SUCCESS");
     }
 }
